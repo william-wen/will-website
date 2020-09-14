75c77ca15251831b9c70df1f98a0d35c515a45cc
William Wen <william.wen360@gmail.com>
2020-09-14 19:03:04 -0400

    Updates

---
+++ precache-manifest.bdfd0fb03c1fb6ef97ed0a6039b22f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4832dbc9908b9a7320f95bfc50cb24",
+    "url": "/will-website2/index.html"
+  },
+  {
+    "revision": "889c132a53b65c82818b",
+    "url": "/will-website2/static/css/main.d00b0e27.chunk.css"
+  },
+  {
+    "revision": "dc4c1c61b0cdbbd6968d",
+    "url": "/will-website2/static/js/2.0b2e1eb1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/will-website2/static/js/2.0b2e1eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889c132a53b65c82818b",
+    "url": "/will-website2/static/js/main.9b26c67c.chunk.js"
+  },
+  {
+    "revision": "ac00c641608be8071cd1",
+    "url": "/will-website2/static/js/runtime-main.9b7150e6.js"
+  }
+]);
